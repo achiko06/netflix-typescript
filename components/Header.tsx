6c3e624170bd980c8d4fe895f99dd739a7f27062
achiko06 <achiko06@gmail.com>
2022-09-06 11:01:18 +0200

    navbar scroll

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,31 @@
 import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 
 
 function Header() {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY > 0) {
+            setIsScrolled(true)
+          } else {
+            setIsScrolled(false)
+          }
+        }
+    
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+        window.removeEventListener('scroll', handleScroll)
+      }
+    }, [])
+
+
   return (
-    <header>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
        <div className="flex items-center space-x-2 md:space-x-10">
         <img
             src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
