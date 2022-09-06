9f24fa3384981ccebbdaf1581ccb71d14646b272
achiko06 <achiko06@gmail.com>
2022-09-06 10:37:56 +0200

    header

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+function Login() {
+  return (
+    <div>
+        <Head>
+        <title>RavenFlix |Login</title>
+      </Head>
+        Login
+        </div>
+  )
+}
+
+export default Login
