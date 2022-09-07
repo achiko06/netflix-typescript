8028159c0121bdf546ce5e3cbd26557a4b24529b
achiko06 <achiko06@gmail.com>
2022-09-07 14:57:04 +0200

    auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import Row from '../components/Row'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
+import useAuth from '../hooks/useAuth'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -26,6 +27,9 @@ const Home = ({
   topRated,
   trendingNow,
 } : Props) => {
+  const { logout, loading } = useAuth()
+
+  if (loading) return null
   
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import useAuth from '../hooks/useAuth'
 
 
 interface Inputs {
@@ -10,6 +12,23 @@ interface Inputs {
 
 function Login() {
   const [login, setLogin] = useState(false)
+  const { signIn, signUp } = useAuth()
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data)
+    if (login) {
+      await signIn(data.email, data.password)
+    } else {
+      await signUp(data.email, data.password)
+    }
+  }
   
 
 
@@ -19,12 +38,14 @@ function Login() {
         <title>Login to RavenFlix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Image
         src="https://rb.gy/p2hphi"
         layout="fill"
         className="-z-10 !hidden opacity-60 sm:!inline"
         objectFit="cover"
       />
+
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
@@ -32,21 +53,51 @@ function Login() {
         height={150}
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <h1 className="text-4xl font-semibold">Sign In</h1>
+        <p>This is NOT the real Netflix, site is built only for personal project demo purporses</p>
+        <p>DO NOT input your real account data</p>
 
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type='email' placeholder='Email' className='input'/>
+            <input
+              type='email'
+              placeholder='Email'
+              className={`input ${
+                errors.email && 'border-b-2 border-orange-500'
+              }`}
+              {...register('email', { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
 
           <label className="inline-block w-full">
-            <input type='password' placeholder='Password' className='input'/>
+            <input 
+              type='password'
+              {...register('password', { required: true })} 
+              placeholder='Password' 
+              className={`input ${
+                errors.password && 'border-b-2 border-orange-500'
+              }`}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
         </div>
 
         <button
           className="w-full rounded bg-[#E50914] py-3 font-semibold"
+          onClick={() => setLogin(true)}
           type="submit"
         >
           Sign In
@@ -56,6 +107,7 @@ function Login() {
           New to Netflix?{' '}
           <button
             className="cursor-pointer text-white hover:underline"
+            onClick={() => setLogin(false)}
             type="submit"
           >
             Sign up now
@@ -64,7 +116,6 @@ function Login() {
 
       </form>
 
-      
     </div>
   )
 }
